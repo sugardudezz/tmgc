d13c65c78c27f86fa02024c8da0b475d155fb19d
windbreakeryubin <phn000000@naver.com>
2024-12-13 01:00:14 +0900

    버튼 배경 추가 정은찬 개색기

--- src/scenes/FightScene.java
+++ src/scenes/FightScene.java
@@ -4,16 +4,23 @@ import scripts.Main;
 import scripts.Storage;
 import tamagochi.Tamagochi;
 import tamagochi.TamagochiButton;
+import utils.BackgroundImage;
 import utils.Geometry;
 import utils.HPBar;
+import java.awt.Color;
+import java.awt.Font;
+import java.util.Random;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class FightScene extends JPanel {
     static TamagochiFighter player;
     static TamagochiFighter enemy;
     static ArrayList<TamagochiFighter> substitutions = new ArrayList<>();
+
+    String imagePath = "./src/images/";
 
     public FightScene(){
         setLayout(null);
@@ -35,6 +42,15 @@ public class FightScene extends JPanel {
         //공격 버튼
         MyButton attack = new MyButton("공격");
         attack.setBounds(new Geometry(0.21f,0.79f,0.2f,0.1f));
+        Geometry.setIcon(attack, imagePath + "AttackPanel.png");
+
+        attack.setForeground(Color.WHITE);  // 텍스트 색상을 흰색으로 설정
+        attack.setHorizontalTextPosition(SwingConstants.CENTER);  // 텍스트 중앙 정렬
+        attack.setVerticalTextPosition(SwingConstants.CENTER);    // 텍스트 중앙 정렬
+
+        Font font = new Font("woodPanel", Font.BOLD, 30);
+        attack.setFont(font);
+
         attack.addActionListener(e -> {
             AttackThread attackThread = new AttackThread(AttackThread.PLAYER);
             attackThread.start();
@@ -44,6 +60,13 @@ public class FightScene extends JPanel {
         //교체 버튼
         MyButton substitute = new MyButton("교체");
         substitute.setBounds(new Geometry(0.5f,0.79f,0.2f,0.1f));
+        Geometry.setIcon(substitute, imagePath + "SwitchPanel.png");
+
+        substitute.setForeground(Color.WHITE);  // 텍스트 색상을 흰색으로 설정
+        substitute.setHorizontalTextPosition(SwingConstants.CENTER);  // 텍스트 중앙 정렬
+        substitute.setVerticalTextPosition(SwingConstants.CENTER);    // 텍스트 중앙 정렬
+        substitute.setFont(font);
+
         substitute.addActionListener(e -> MyButton.setSubstitution(true));
         substitute.setName("substitute");
         add(substitute);
@@ -79,6 +102,13 @@ public class FightScene extends JPanel {
         //도주 버튼
         MyButton flee = new MyButton("도주");
         flee.setBounds(new Geometry(0.79f,0.79f,0.2f,0.1f));
+        Geometry.setIcon(flee, imagePath + "RunPanel.png");
+
+        flee.setForeground(Color.WHITE);  // 텍스트 색상을 흰색으로 설정
+        flee.setHorizontalTextPosition(SwingConstants.CENTER);  // 텍스트 중앙 정렬
+        flee.setVerticalTextPosition(SwingConstants.CENTER);    // 텍스트 중앙 정렬
+        flee.setFont(font);
+
         flee.addActionListener(e -> {
             Main.round++;
             SceneManager.playScene(new RoundScene());
@@ -95,6 +125,21 @@ public class FightScene extends JPanel {
         if(enemy.tamagochi.isFaster(player.tamagochi)){
             AttackThread attackThread = new AttackThread(AttackThread.ENEMY);
             attackThread.start();
+        }
+
+        CheckRound();
+    }
+
+    public void CheckRound() {
+        if ( Main.round >= 0 && Main.round <= 10 ) {
+            BackgroundImage bg = new BackgroundImage("./src/images/FightScene1.png"); add(bg);
+        }
+        else if ( Main.round >= 11 && Main.round <= 20 ) {
+            BackgroundImage bg = new BackgroundImage("./src/images/FightScene2.png"); add(bg);
+        }
+
+        else {
+            BackgroundImage bg = new BackgroundImage( "./src/images/FightScene3.png"); add(bg);
         }
     }
 
